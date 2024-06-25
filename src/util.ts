e3f124f06e6ecf64eddad3edca3d57f3c9d39a96
Ivy Rose <hi@ivyrose.me>
2024-06-25 17:31:06 -0400

    Refactor reference resolving to utility function

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,16 @@
+import { sanityClient } from "sanity:client";
+import groq from "groq";
+import type { SanityReference } from "@sanity/client";
+
 export const generateDatedPostSlug = (datePublished: string, baseSlug: string) => {
     const date = new Date(datePublished);
     const dateFormatted = date.toISOString().split('T')[0].replaceAll('-', '/');
     return `${dateFormatted}/${baseSlug}`;
+};
+
+export const resolveSanityRef = <T>(ref: SanityReference) => {
+    const resolveRefQuery = groq`
+        *[_id == $id][0]
+    `;
+    return sanityClient.fetch<T>(resolveRefQuery, { id: ref._ref });
 };
