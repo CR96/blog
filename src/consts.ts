42b66bd1b0563c6c96b8dfe0bfd8cf211c4d95aa
Ivy Rose <hi@ivyrose.me>
2024-06-28 19:58:46 -0400

    Use site title for navigation logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Café Car: Mode Shift in the Mitten';
+export const SITE_TITLE = 'The Café Car';
 export const SITE_DESCRIPTION = 'A Michigan transit advocacy and travel blog by Corey Rowe';
