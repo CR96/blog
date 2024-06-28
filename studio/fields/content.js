afe9a426582541529e870810411f915135a030bb
Ivy Rose <hi@ivyrose.me>
2024-06-28 19:42:35 -0400

    Fix disconnected adjacent opinion blocks

---
+++ studio/fields/content.js
@@ -0,0 +1,115 @@
+import alt from '../fields/alt';
+import caption from '../fields/caption';
+import block from '../fields/block';
+import { BookIcon, ImageIcon, PresentationIcon, ThListIcon, TwitterIcon } from "@sanity/icons";
+
+export default {
+  name: "content",
+  title: "Content",
+  type: "array",
+  of: [
+    {
+      ...block,
+      marks: {
+        annotations: [
+          {
+            name: "link",
+            title: "Link",
+            type: "object",
+            fields: [
+              {
+                name: "href",
+                title: "Link",
+                description: "A valid web, email, phone, or relative link.",
+                type: "url",
+                validation: rule => rule.uri({
+                  allowRelative: true
+                }).required()
+              }
+            ]
+          },
+          {
+            name: "subject",
+            title: "Subject",
+            type: "object",
+            icon: BookIcon,
+            fields: [
+              {
+                name: "subject",
+                title: "Subject",
+                type: "reference",
+                to: [
+                  { type: "subject" }
+                ],
+                validation: rule => rule.required()
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      fields: [
+        alt,
+        caption
+      ],
+      icon: ImageIcon
+    },
+    {
+      name: 'video',
+      title: 'Video',
+      type: 'object',
+      icon: PresentationIcon,
+      fields: [
+        {
+          name: 'video',
+          type: 'mux.video',
+          title: 'Video',
+          validation: rule => rule.required()
+        },
+        caption
+      ]
+    },
+    {
+      name: "tweet",
+      title: "Tweet",
+      type: "object",
+      fields: [
+        {
+          name: "id",
+          title: "Tweet ID",
+          type: "string",
+          validation: rule => rule.required()
+        },
+        caption
+      ],
+      icon: TwitterIcon,
+    },
+    {
+      name: "table",
+      title: "Table",
+      type: "object",
+      fields: [
+        {
+          name: "table",
+          title: "Table Body",
+          type: "table",
+          validation: rule => rule.required()
+        },
+        {
+          name: "numHeader",
+          title: "Table Header",
+          description: "Number of rows that represent the table header",
+          type: "number",
+          initialValue: 0,
+          validation: rule => rule.required().min(0)
+        }
+      ],
+      icon: ThListIcon
+    }
+  ],
+  validation: rule => rule.required()
+}
--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -1,7 +1,7 @@
-import block from '../fields/block';
 import alt from '../fields/alt';
 import caption from '../fields/caption';
-import { BookIcon, ImageIcon, PresentationIcon, ThListIcon, TwitterIcon } from "@sanity/icons";
+import content from '../fields/content';
+import { BulbOutlineIcon } from '@sanity/icons';
 
 export default {
   name: "post",
@@ -57,118 +57,19 @@ export default {
       ]
     },
     {
-      name: "content",
-      title: "Content",
-      type: "array",
+      ...content,
       of: [
+        ...content.of,
         {
-          ...block,
-          styles: [
-            ...block.styles,
-            { title: 'Opinion', value: 'opinion' }
-          ],
-          marks: {
-            annotations: [
-              {
-                name: "link",
-                title: "Link",
-                type: "object",
-                fields: [
-                  {
-                    name: "href",
-                    title: "Link",
-                    description: "A valid web, email, phone, or relative link.",
-                    type: "url",
-                    validation: rule => rule.uri({
-                      allowRelative: true
-                    }).required()
-                  }
-                ]
-              },
-              {
-                name: "subject",
-                title: "Subject",
-                type: "object",
-                icon: BookIcon,
-                fields: [
-                  {
-                    name: "subject",
-                    title: "Subject",
-                    type: "reference",
-                    to: [
-                      { type: "subject" }
-                    ],
-                    validation: rule => rule.required()
-                  }
-                ]
-              }
-            ]
-          }
-        },
-        {
-          name: "image",
-          title: "Image",
-          type: "image",
-          fields: [
-            alt,
-            caption
-          ],
-          icon: ImageIcon
-        },
-        {
-          name: 'video',
-          title: 'Video',
-          type: 'object',
-          icon: PresentationIcon,
-          fields: [
-            {
-              name: 'video',
-              type: 'mux.video',
-              title: 'Video',
-              validation: rule => rule.required()
-            },
-            caption
-          ]
-        },
-        {
-          name: "tweet",
-          title: "Tweet",
+          name: "opinion",
+          title: "Opinion",
           type: "object",
           fields: [
-            {
-              name: "id",
-              title: "Tweet ID",
-              type: "string",
-              validation: rule => rule.required()
-            },
-            caption
+            content
           ],
-          icon: TwitterIcon,
-        },
-        {
-          name: "table",
-          title: "Table",
-          type: "object",
-          fields: [
-            {
-              name: "table",
-              title: "Table Body",
-              type: "table",
-              validation: rule => rule.required()
-            },
-            {
-              name: "numHeader",
-              title: "Table Header",
-              description: "Number of rows that represent the table header",
-              type: "number",
-              initialValue: 0,
-              validation: rule => rule.required().min(0)
-            }
-          ],
-          icon: ThListIcon
+          icon: BulbOutlineIcon,
         }
-      ],
-      validation: rule => rule.required()
+      ]
     }
   ]
 }
