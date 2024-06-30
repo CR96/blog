84cbab5ee4e232aa62abd412ea932cb4040b2953
Ivy Rose <hi@ivyrose.me>
2024-06-30 15:37:20 -0400

    Add simple post preview endpoint

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,9 @@ import { muxInput } from "sanity-plugin-mux-input";
 import { table } from "@sanity/table";
 import schemas from "./studio/schemas/schema";
 import deskStructure from "./studio/desk-structure";
+import { generateDatedPostSlug } from "./src/util";
+import type { Post } from "./src/groq";
+import groq from "groq";
 
 export default defineConfig({
   name: "blog",
@@ -19,6 +22,22 @@ export default defineConfig({
   ],
   schema: {
     types: schemas,
+  },
+  document: {
+    productionUrl: async (prev, context) => {
+      const { document, getClient } = context;
+
+      if (document._type != 'post') { return prev; }
+
+      const client = getClient({ apiVersion: 'v2022-06-30' });
+      const postUrlQuery = groq`
+        *[_type == 'post' && _id == $id][0]
+      `;
+      const post = await client.fetch<Post>(postUrlQuery, { id: document._id });
+      const datedSlug = generateDatedPostSlug(post.datePublished, post.slug.current);
+
+      return `/${datedSlug}/preview`;
+    }
   },
   basePath: "/admin"
 });
