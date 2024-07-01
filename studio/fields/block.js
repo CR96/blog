606f00ebf98259d80dd5542a3bf15cac2dcf062b
Ivy Rose <hi@ivyrose.me>
2024-07-01 18:47:13 -0400

    Add blockquotes to content editor

--- studio/fields/block.js
+++ studio/fields/block.js
@@ -5,6 +5,7 @@ export default {
         { title: 'Heading 3', value: 'h3' },
         { title: 'Heading 4', value: 'h4' },
         { title: 'Heading 5', value: 'h5' },
-        { title: 'Heading 6', value: 'h6' }
+        { title: 'Heading 6', value: 'h6' },
+        { title: 'Blockquote', value: 'blockquote' }
     ]
 };
