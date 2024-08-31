1171f1c50b4f9f75252919c7e90436e5731f1ed7
Corey Rowe <coreyr1196@gmail.com>
2024-08-31 07:30:41 -0400

    Use constants for short and full blog name

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'The Café Car';
+export const SITE_LONG_TITLE = 'The Café Car: Mode Shift in the Mitten';
 export const SITE_DESCRIPTION = 'A Michigan transit advocacy and travel blog by Corey Rowe';
